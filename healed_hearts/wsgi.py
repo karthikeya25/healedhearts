cdad3dbb2bd97521cfe81c8fe59b8e180ccda9d6
Karthikeya Nagisetti <61624986+karthikeya25@users.noreply.github.com>
2025-01-29 11:35:52 -0600

    Update wsgi.py

--- healed_hearts/wsgi.py
+++ healed_hearts/wsgi.py
@@ -1,5 +1,5 @@
 
-from app import app, socketio
+from app import app
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080)
